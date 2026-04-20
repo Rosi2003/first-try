f1d9da699d71d1fa816358af14c370c42d074af0
Namadee Umedya <namadeeumedya2002@gmail.com>
2026-04-20 10:50:13 +0530

    Update calculator with comments

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+# This is a Simple Calculator project created in a new branch
 print("Simple Calculator")
 
 n1 = float(input("Enter first number: "))
@@ -5,14 +6,14 @@ op = input("Enter operator (+, -, *, /): ")
 n2 = float(input("Enter second number: "))
 
 if op == "+":
-    print("Answer: ",n1+n2)
+    print(f"--- Result: {n1 + n2} ---")
 elif op == "-":
-    print("Answer: ",n1-n2)
+    print(f"--- Result: {n1 - n2} ---")
 elif op == "*":
-    print("Answer: ",n1*n2)
+    print(f"--- Result: {n1 * n2} ---")
 elif op == "/":
     if n2 != 0:
-        print("Answer: ",n1/n2)
+        print(f"--- Result: {n1 / n2} ---")
     else:
         print("Cannot divide by zero")
 else:
